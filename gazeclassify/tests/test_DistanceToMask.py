2d85528549ed791087e1979ef430fe640fee7b35
Daniel Müller <email.daniel.ch@gmail.com>
2021-05-07 13:46:45 +0200

    Create test for distance to shape

---
+++ gazeclassify/tests/test_DistanceToMask.py
@@ -0,0 +1,25 @@
+import numpy as np  # type: ignore
+
+class Test_Measuring2DDistanceGazeTo_Shape:
+    def test_read_boolean_2D_mask_and_calculate_distance_gaze_to_closest_pixel(self) -> None:
+        image_mask = np.zeros((3, 3))
+        image_mask[0, 0] = 1
+        image_mask[0, 1] = 1
+        gaze = [[2, 0]]
+
+        detected_shape = np.argwhere(image_mask == 1)
+        dist_2 = np.sqrt(np.sum((detected_shape - gaze) ** 2, axis=1))
+        distance = np.argmin(dist_2)
+        assert distance == 2
+
+    def test_read_boolean_2D_mask_and_identify_distance_to_gaze_should_return_sqrt2_when_diagnoally(self) -> None:
+        mask = np.array(
+            [
+                [1, 0],
+                [0, 0]
+            ]
+        )
+        detected_shape = np.argwhere(mask == 1)
+        gaze = [[1, 1]]
+        distance = np.linalg.norm(detected_shape - gaze)
+        assert distance == np.sqrt(2)
--- gazeclassify/thirdparty/colab/facade.py
+++ gazeclassify/thirdparty/colab/facade.py
@@ -8,7 +8,6 @@ from pixellib.instance import instance_segmentation, configuration  # type: igno
 from gazeclassify.core.model.dataset import Dataset
 from gazeclassify.serializer.pupil_repository import PupilInvisibleRepository
 from gazeclassify.serializer.pupil_serializer import PupilDataSerializer
-from gazeclassify.thirdparty.opencv import OpenCVFrameReader
 from gazeclassify.thirdparty.pixellib.helpers import InferSpeed
 
 
@@ -24,53 +23,69 @@ def load_from_pupil_invisible(path: str) -> Dataset:
 
 def classify(dataset: Dataset, name: str) -> None:
     source_file = dataset.world_video.file
-    frame_reader = OpenCVFrameReader(source_file)
 
-    segment_image = instance_segmentation(infer_speed=InferSpeed.FAST.value)
+    segment_image = instance_segmentation(infer_speed=InferSpeed.AVERAGE.value)
     model_path = os.path.expanduser("~/gazeclassify_data/mask_rcnn_coco.h5")
     segment_image.load_model(model_path)
     target_classes = segment_image.select_target_classes(person=True)
 
-    frame_reader.open_capture()
+    # SEND FRAME TO WRITER
+    video_target = os.path.expanduser(f"~/gazecalssify_data/{name}.avi")
+    result_video = cv2.VideoWriter("person.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10,
+                                   (dataset.world_video.width, dataset.world_video.height))
 
+    capture = cv2.VideoCapture(str(source_file))
     for record in dataset.records:
-        frame = frame_reader.get_frame()
 
-        # Reconvert to cv2
-        reconvert = frame.getvalue()  # type: ignore
-        image = cv2.imdecode(np.frombuffer(reconvert, np.uint8), cv2.IMREAD_COLOR)
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        hasframe, frame = capture.read()
 
-        # segment
-        segmask, output = segment_image.segmentImage(
-            rgb_image,
-            segment_target_classes=target_classes,
-            process_frame=True)
-        img_converted = Image.fromarray(output)
-        img_converted.show()
+        if not hasframe:
+            print("Ran out of frames")
 
-        # Only get the masks for person
+        segmask, output = segment_image.segmentFrame(frame, segment_target_classes=target_classes)
+
+        # img_converted = Image.fromarray(output)
+        # img_converted.show()
+
+        # Only get the masks for all areas of type person
         people_masks = []
         person_class_id = 1
-        for index, mask in enumerate(segmask["masks"]):
+        for index in range(segmask["masks"].shape[-1]):
             if segmask['class_ids'][index] == person_class_id:
-                people_masks.append(mask)
+                people_masks.append(segmask["masks"][:, :, index])
 
         # concatenate all masks (not distinguishing between codes)
         bool_concat = np.any(segmask["masks"], axis=-1)
         int_concat = bool_concat.astype(int) * 255
 
         # Extend to 3d rgb image
-        b = np.repeat(int_concat[:, :, np.newaxis], 3, axis=2)
-        Image.fromarray(b.astype(np.uint8)).show()
+        # b = np.repeat(int_concat[:, :, np.newaxis], 3, axis=2)
+        # Image.fromarray(b.astype(np.uint8)).show()
 
-        bytesframe = cv2.imencode('.jpg', int_concat)[1].tobytes()
+        # bytesframe = cv2.imencode('.jpg', int_concat)[1].tobytes()
+        #
+        # # Append to writer
+        # bytesframe = cv2.imencode('.jpg', int_concat)[1]
+        #
+        # image = Image.fromarray(b.astype(np.uint8))
 
-        # SEND FRAME TO WRITER
+        lower_int = int_concat.astype('uint8')
 
+        a = 1
+
+        rgb_out = np.dstack([lower_int]*3)
+
+        result_video.write(rgb_out)
+
+
+        break
         # QUERY GAZE: is on image?
 
         # Save result to dataset results
+
+    result_video.release()
+    capture.release()
+    cv2.destroyAllWindows()
 
 
 dataset = load_from_pupil_invisible("gazeclassify/tests/data")
