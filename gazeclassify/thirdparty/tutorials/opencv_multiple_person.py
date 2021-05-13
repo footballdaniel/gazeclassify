d11dc859e48c45dd5735fa11daff293f7ff49886
Daniel Müller <email.daniel.ch@gmail.com>
2021-05-13 14:32:04 +0200

    multiple keypoints identified

---
+++ gazeclassify/thirdparty/tutorials/opencv_multiple_person.py
@@ -0,0 +1,49 @@
+# https://github.com/spmallick/learnopencv/blob/master/OpenPose/OpenPoseImage.py
+import os
+
+import cv2  # type: ignore
+import numpy as np  # type: ignore
+from matplotlib import pyplot as plt  # type: ignore
+
+from gazeclassify.core.services.analysis import ModelLoader
+
+ModelLoader("http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel",
+            "~/gazeclassify_data/").download_if_not_available("pose_iter_160000.caffemodel")
+protoFile = os.path.expanduser("~/gazeclassify_data/") + "pose_deploy_linevec.prototxt"
+weightsFile = os.path.expanduser("~/gazeclassify_data/") + "pose_iter_440000.caffemodel"
+
+nPoints = 18
+POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
+              [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
+
+frame = cv2.imread("../../tests/data/humans.jpeg")
+frameCopy = np.copy(frame)
+frameWidth = frame.shape[1]
+frameHeight = frame.shape[0]
+threshold = 0.05
+
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+# if args.device == "cpu":
+net.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
+# elif args.device == "gpu":
+#     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+#     print("Using GPU device")
+
+# input image dimensions for the network
+inWidth = 368
+inHeight = 368
+inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
+                                (0, 0, 0), swapRB=False, crop=False)
+
+net.setInput(inpBlob)
+output = net.forward() # shape is 1,keypoints,pixelwidht, pixelheight
+
+H = output.shape[2]
+W = output.shape[3]
+
+i = 0 # is the head
+probMap = output[0, i, :, :]
+
+
--- gazeclassify/thirdparty/tutorials/opencv_multiple_person_probability_map.py
+++ gazeclassify/thirdparty/tutorials/opencv_multiple_person_probability_map.py
@@ -1,8 +1,9 @@
 # https://github.com/spmallick/learnopencv/blob/master/OpenPose/OpenPoseImage.py
 import os
 
-import cv2
-import numpy as np
+import cv2  # type: ignore
+import numpy as np  # type: ignore
+from matplotlib import pyplot as plt  # type: ignore
 
 from gazeclassify.core.services.analysis import ModelLoader
 
@@ -37,43 +38,16 @@ inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
                                 (0, 0, 0), swapRB=False, crop=False)
 
 net.setInput(inpBlob)
-output = net.forward()
+output = net.forward() # shape is 1,keypoints,pixelwidht, pixelheight
 
 H = output.shape[2]
 W = output.shape[3]
 
-# Empty list to store the detected keypoints
-points = []
+i = 0 # is the head
+probMap = output[0, i, :, :] #
 
-for i in range(nPoints):
-    # confidence map of corresponding body's part.
-    probMap = output[0, i, :, :]
-
-    # Find global maxima of the probMap.
-    minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-
-    # Scale the point to fit on the original image
-    x = (frameWidth * point[0]) / W
-    y = (frameHeight * point[1]) / H
-
-    if prob > threshold:
-        cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-        cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
-                    lineType=cv2.LINE_AA)
-
-        # Add the point to the list if the probability is greater than the threshold
-        points.append((int(x), int(y)))
-    else:
-        points.append(None)
-
-# Draw Skeleton
-for pair in POSE_PAIRS:
-    partA = pair[0]
-    partB = pair[1]
-
-    if points[partA] and points[partB]:
-        cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
-        cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-cv2.imshow('Output-Skeleton', frame)
-cv2.waitKey(0)
+# show image
+probMap = cv2.resize(probMap, (frameWidth, frameHeight))
+plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+plt.imshow(probMap, alpha=0.6)
+plt.show()
